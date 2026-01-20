8704b5f4a5f33f17e6e4750b2bc78621cf63adf1
WolfShizu <ht0981.2005@gmail.com>
2026-01-20 00:46:38 -0300

    fix: Correção na importação do client e tipo do discord_token

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import os
 import asyncio
 from dotenv import load_dotenv
 
-from discord_client import DiscordClient
+from app.discord_client import DiscordClient
 
 load_dotenv()
-discord_token = os.getenv("DISCORD_TOKEN")
+discord_token = str(os.getenv("DISCORD_TOKEN"))
 
 bot = DiscordClient()
 
 # roda o bot de forma assíncrona
 async def main():
     async with bot:
-        await bot.start(discord_token)
+        await bot.start(token= discord_token)
 
 asyncio.run(main())
