c98a9d7e0dc174278e9f1e71d259bae5986cd5f9
WolfShizu <ht0981.2005@gmail.com>
2026-01-23 15:36:15 -0300

    feat: Cria o gatekeeper com estrutura básica de histórico de prefixo e permissões de chat por servidor

---
+++ app/gatekeeper.py
@@ -0,0 +1,31 @@
+class Gatekeeper:
+    def __init__(self, default_prefix: str = "!"):
+        self.default_prefix = default_prefix
+
+        # TODO Buscar informação do banco de dados
+        self.guild_prefix_map = {
+            0000: "!"
+        }
+
+        # TODO Buscar informação do banco de dados
+        self.channels_access_map = {
+            0000: {                     # ID do chat
+                "allowed_roles": [
+                    1000,
+                    1001,
+                    1002
+                ],
+                "denied_roles": [
+                    1003,
+                    1004
+                ],
+                "allowed_users": [
+                    2000,
+                    2001
+                ],
+                "denied_users": [
+                    3001,
+                    3002
+                ]
+            }
+        }
