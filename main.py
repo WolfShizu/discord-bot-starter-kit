35b1e87f2a4a2b4d3b25e7b80fcf71d4b9ca4c3a
WolfShizu <ht0981.2005@gmail.com>
2026-01-23 00:27:01 -0300

    feat: Adiciona o message_handler e o injeta no bot_instance

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import asyncio
 from dotenv import load_dotenv
 
 from app.discord_client import DiscordClient
+from app.message_handler import MessageHandler
 
 load_dotenv()
 discord_token = str(os.getenv("DISCORD_TOKEN"))
 
-bot = DiscordClient()
+message_handler = MessageHandler()
+bot_instance = DiscordClient(message_handler)
 
 # roda o bot de forma assíncrona
 async def main():
-    async with bot:
-        await bot.start(token= discord_token)
+    async with bot_instance:
+        await bot_instance.start(token= discord_token)
 
 asyncio.run(main())
