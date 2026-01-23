33e6813fbad2e9f0ecaec1d8a837811a3a0c9653
WolfShizu <ht0981.2005@gmail.com>
2026-01-23 17:09:40 -0300

    feat: Adiciona ao payload da mensagem do usuário as informações de autorização e se ela é de uma dm

--- app/models/message_payload.py
+++ app/models/message_payload.py
@@ -13,7 +13,13 @@ class UserMessagePayload:
     message: Message
     send_message_function: Callable[[BotResponsePayload], Awaitable[Any]]
 
+    is_private_message: Optional[bool] = None
+
     raw_message: Optional[str] = None
     is_command: Optional[bool] = None
     command_name: Optional[str] = None
     arguments: Optional[list[str]] = None
+
+    is_admin_role: Optional[bool] = None
+    is_ahthorized_role: Optional[bool] = None
+    is_authorized_user: Optional[bool] = None
