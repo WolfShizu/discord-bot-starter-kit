ee3d933b55586e3ae238d963224659269b73b234
WolfShizu <ht0981.2005@gmail.com>
2026-01-22 23:54:41 -0300

    fix: Correção no tipo da função de enviar mensagem

--- app/models/message_payload.py
+++ app/models/message_payload.py
@@ -4,17 +4,17 @@ from typing import Optional, Any, Callable
 from discord import Message, TextChannel, Embed
 
 @dataclass
+class BotResponsePayload:
+    content: str
+    channel: Optional[TextChannel] = None # Envia no canal atual
+    embed: Optional[Embed] = None
+
+@dataclass
 class UserMessagePayload:
     message: Message
-    send_message_function: Callable[[str], Any]
+    send_message_function: Callable[[BotResponsePayload], Any]
 
     raw_message: Optional[str] = None
     is_command: Optional[bool] = None
     command_name: Optional[str] = None
     arguments: Optional[list[str]] = None
-
-@dataclass
-class BotResponsePayload:
-    content: str
-    channel: Optional[TextChannel] = None # Envia no canal atual
-    embed: Optional[Embed] = None
