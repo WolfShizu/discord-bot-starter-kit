4cea9b823ec3103dfeae3bcd20bc5651833935f9
WolfShizu <ht0981.2005@gmail.com>
2026-01-31 16:43:51 -0300

    feat: Guarda a função de envio de mensagem no objeto

--- app/commands/base.py
+++ app/commands/base.py
@@ -1,19 +1,23 @@
 from abc import ABC, abstractmethod
+from typing import Any, Callable, Coroutine
 
 from app.models.message_payload import UserMessagePayload
 
 class BaseCommand(ABC):
-    def __init__(self):
+    def __init__(self, send_message_function: Callable[[Any, Any], Coroutine[Any, Any, None]]):
         self.name: str = ""
         self.aliases: list[str] = []
+
+        self.send_message_function = send_message_function
+
 
     @abstractmethod
     async def execute(self, payload: UserMessagePayload) -> None:
         pass
     
 class BaseListener(ABC):
-    def __init__(self):
-        pass
+    def __init__(self, send_message_function: Callable[[Any, Any], Coroutine[Any, Any, None]]):
+        self.send_message_function = send_message_function
 
     @abstractmethod
     async def handle_event(self, payload: UserMessagePayload) -> None:
