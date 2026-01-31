64f32c59b51d4acd144f21132bd12877b00076d4
WolfShizu <ht0981.2005@gmail.com>
2026-01-31 16:23:55 -0300

    feat: Verifica se o comando ou o listener já foram registrados

--- app/dispatcher.py
+++ app/dispatcher.py
@@ -1,36 +1,61 @@
 # TODO Configurar o event type do listener
 
-from typing import Any
+from typing import Any, Literal
 
 from app.commands.base import BaseCommand, BaseListener
 
 class Dispatcher:
     def __init__(self):
-        self.commands: dict[str, Any] = {}
-        self.listeners: list[Any] = []
+        self.commands_map: dict[str, Any] = {}
+        self.listeners_list: list[Any] = []
 
+    # <---- Decorador para registrar comandos ---->
     def register_command(self, command_name: str, command_aliases: list[str]):
         def wrapper(command_class: type[BaseCommand]):
             command_instance = command_class()
 
             command_instance.name = command_name.lower()
             command_instance.aliases = [command.lower() for command in command_aliases]
+            
+            if self._verifiy_command_exists(command_instance.name):
+                raise ValueError(f"Comando '{command_instance.name}' já registrado.") # TODO Alterar para exceção personalizada
 
-            # TODO Fazer uma verificação no self.commands. Verificar se o comando já foi registrado. Se sim, deve retornar um erro
-            self.commands[command_instance.name] = command_instance
+            self.commands_map[command_instance.name] = command_instance
 
             for alias in command_instance.aliases:
-                self.commands[alias] = command_instance
+                if self._verifiy_command_exists(alias):
+                    raise ValueError(f"Alias '{alias}' já registrado.") # TODO Alterar para exceção personalizada
+                self.commands_map[alias] = command_instance
 
             return command_class
             
         return wrapper
     
+    def _verifiy_command_exists(self, command_name: str, ) -> bool:
+
+        command = self.commands_map.get(command_name.lower())
+
+        if command:
+            return True
+        
+        return False
+    
+    # <---- Decorador para registrar listeners ---->
     def register_listener(self):
         def wrapper(command_class: type[BaseListener]):
             command_instance = command_class()
 
-            self.listeners.append(command_instance)
+            if self._verify_listener_exists(command_class):
+                raise ValueError(f"Listener '{command_class.__name__}' já registrado.") # TODO Alterar para exceção personalizada
+
+            self.listeners_list.append(command_instance)
 
             return command_class
         return wrapper
+    
+    def _verify_listener_exists(self, listener_class: type[BaseListener]) -> bool:
+        for listener in self.listeners_list:
+            if isinstance(listener, listener_class):
+                return True
+        
+        return False
