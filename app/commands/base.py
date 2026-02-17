445ccdd479bb9ccde28c16dc33742805208ac2e1
WolfShizu <ht0981.2005@gmail.com>
2026-02-17 16:17:59 -0300

    refactor: remove o send_message da classe base de comando e listener

--- app/commands/base.py
+++ app/commands/base.py
@@ -7,18 +7,11 @@ class BaseCommand(ABC):
     command_name: str = ""
     command_aliases: list[str] = []
 
-    def __init__(self, send_message_function: Callable[[Any, Any], Coroutine[Any, Any, None]]):
-        self.send_message_function = send_message_function
-
-
     @abstractmethod
     async def execute_command(self, payload: UserMessagePayload) -> None:
         pass
 
 class BaseListener(ABC):
-    def __init__(self, send_message_function: Callable[[Any, Any], Coroutine[Any, Any, None]]):
-        self.send_message_function = send_message_function
-
     @abstractmethod
     async def handle_event(self, payload: UserMessagePayload) -> None:
         pass
