605230e690f8a64f424dd13cb4cdeb845f5df504
WolfShizu <ht0981.2005@gmail.com>
2026-02-17 14:57:30 -0300

    refactor: move o nome e aliases do comando para os atributos da classe

--- app/commands/base.py
+++ app/commands/base.py
@@ -4,17 +4,17 @@ from typing import Any, Callable, Coroutine
 from app.models.message_payload import UserMessagePayload
 
 class BaseCommand(ABC):
-    def __init__(self, send_message_function: Callable[[Any, Any], Coroutine[Any, Any, None]]):
-        self.name: str = ""
-        self.aliases: list[str] = []
+    command_name: str = ""
+    command_aliases: list[str] = []
 
+    def __init__(self, send_message_function: Callable[[Any, Any], Coroutine[Any, Any, None]]):
         self.send_message_function = send_message_function
 
 
     @abstractmethod
     async def execute_command(self, payload: UserMessagePayload) -> None:
         pass
-    
+
 class BaseListener(ABC):
     def __init__(self, send_message_function: Callable[[Any, Any], Coroutine[Any, Any, None]]):
         self.send_message_function = send_message_function
