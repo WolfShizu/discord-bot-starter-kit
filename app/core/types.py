175d01e3dc624f3d118ea4424aeda5b3bfafbc9f
WolfShizu <ht0981.2005@gmail.com>
2026-03-01 02:12:04 -0300

    feat: novo enum para severidade dos erros

--- app/core/types.py
+++ app/core/types.py
@@ -3,3 +3,13 @@ from enum import Enum, auto
 class FeatureType(Enum):
     COMMAND = auto()
     LISTENER = auto()
+
+class ExceptionSeverity(Enum):
+    LOW = auto()
+    """Notifica apenas no terminal. Erro aceitável e esperado."""
+
+    MEDIUM = auto()
+    """Notifica e guarda o erro. Funcionalidade afetada, exige correção."""
+
+    CRITICAL = auto()
+    """Notifica, guarda o erro e encerra o processo para evitar corrupção de dados."""
