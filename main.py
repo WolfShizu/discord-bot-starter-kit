daa89fe5bd1f962fbdc8ff012af786d8a7c9902a
WolfShizu <ht0981.2005@gmail.com>
2026-03-12 18:58:27 -0300

    refactor: cria a classe de telemetria no main e remove o log de erro ao desligar o bot

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ from dotenv import load_dotenv
 
 from app.discord_client import DiscordClient
 
+# <--- Telemetria e terminal --->
+from rich.live import Live
+
+from app.core.telemetry import Telemetry, TerminalDashboard
+
+
 load_dotenv()
 discord_token = str(os.getenv("DISCORD_TOKEN"))
 
-bot_instance = DiscordClient()
+telemetry = Telemetry()
+
+bot_instance = DiscordClient(telemetry)
 
 # roda o bot de forma assíncrona
 async def main():
     async with bot_instance:
         await bot_instance.start(token= discord_token)
 
-asyncio.run(main())
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    pass
