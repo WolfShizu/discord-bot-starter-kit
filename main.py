f0eb839eff68458f9f19eb345ca51e2257947abf
WolfShizu <ht0981.2005@gmail.com>
2026-03-20 01:19:24 -0300

    feat: adiciona o objeto de telemetria ao dashboard

--- main.py
+++ main.py
@@ -34,6 +34,9 @@ statistics = SystemStatistics(
 
 telemetry = Telemetry(dashboard, statistics)
 
+# TODO Criar uma classe "mediadora" entre as duas, que contenha todas as funções que uma classe precisa usar da outra
+dashboard.get_telemetry(telemetry)
+
 bot_instance = DiscordClient(dashboard, telemetry, statistics)
 
 # roda o bot de forma assíncrona
