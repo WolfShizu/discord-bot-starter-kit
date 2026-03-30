d33764213e8ecf2d24101b430612eeb62ea0fbd4
WolfShizu <ht0981.2005@gmail.com>
2026-03-29 22:41:35 -0300

    feat: nova função para carregar os modelos do database

--- app/services/database/base/engine.py
+++ app/services/database/base/engine.py
@@ -1,4 +1,6 @@
 import os
+import importlib
+import pkgutil
 from contextlib import asynccontextmanager
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
@@ -29,6 +31,25 @@ async def get_db_session():
     finally:
         await session.close()
 
+def load_models():
+    """Importa todos os módulos na pasta models"""
+    models_path = os.path.join(os.path.dirname(__file__), "models")
+    package_prefix = "app.services.database.base.models"
+
+    for _, module_name, is_package in pkgutil.iter_modules([models_path]):
+        if is_package:
+            module_path = f"{package_prefix}.{module_name}"
+            try:
+                _ = importlib.import_module(module_path)
+            except Exception as error:
+                # TODO Tratar o erro
+                pass
+
+async def setup_database():
+    load_models()
+
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all)
 
 class Base(DeclarativeBase):
     pass
